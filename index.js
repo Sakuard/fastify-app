a397104d48c9ad6f0f2c446324b1b3d9af1db389
Matt.tw <matt.web.tw@gamil.com>
2023-04-12 19:47:11 +0800

    adding swagger,reset the fastify and fastify-swagger version due to not supporting old method

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const fastify = require('fastify')({logger: true});
-fastify.register(require('./routes/items'));
 require('dotenv').config();
+const fastify = require('fastify')({ logger: true })
+fastify.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/docs',
+  swagger: {
+    info: { title: 'fastify-api' },
+  },
+})
+fastify.register(require('./routes/items'))
+
 const PORT = process.env.PORT;
-// const PORT = 5000
 
 const start = async () => {
   try {
     await fastify.listen(PORT)
-
   } catch (error) {
     fastify.log.error(error)
     process.exit(1)
